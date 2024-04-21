fc7c22b055e6da6f51a807adc78fd82b167a99f0
Andrey Chertykovtsev <chertiav@gmail.com>
2024-04-21 14:35:32 +0200

    created theme-provider:  - implement theme provider logic;  - created the theme switcher component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 //===============================================
@@ -7,26 +8,32 @@ import LayoutComponent from './components/layout';
 import User from './pages/user';
 import Home from './pages/home';
 import PrivateRoute from './utils/router/privateRoute';
+import { ColorModeContext, useMode } from './theme';
 
 function App(): React.JSX.Element {
+	const [theme, colorMode] = useMode();
 	return (
-		<>
-			<ToastContainer
-				position={'bottom-right'}
-				hideProgressBar={false}
-				closeOnClick
-				rtl={false}
-				limit={1}
-			/>
-			<Routes>
-				<Route element={<LayoutComponent />}>
-					<Route element={<PrivateRoute />}>
-						<Route path=":username" element={<User />} />
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<ToastContainer
+					position={'bottom-right'}
+					hideProgressBar={false}
+					closeOnClick
+					rtl={false}
+					limit={1}
+					theme={theme.palette.mode === 'dark' ? 'light' : 'dark'}
+				/>
+				<CssBaseline />
+				<Routes>
+					<Route element={<LayoutComponent />}>
+						<Route element={<PrivateRoute />}>
+							<Route path=":username" element={<User />} />
+						</Route>
+						<Route path="/" element={<Home />} />
 					</Route>
-					<Route path="/" element={<Home />} />
-				</Route>
-			</Routes>
-		</>
+				</Routes>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
 	);
 }
 
--- src/components/username/styles.ts
+++ src/components/username/styles.ts
@@ -1,25 +1,34 @@
 import { makeStyles } from 'tss-react/mui';
+//========================================
+import { tokens } from '../../theme';
 
-export const useStyles = makeStyles()({
-	root: {
-		textAlign: 'center',
-		fontFamily: 'Popins',
-		fontSize: '32px',
-	},
-	description: {
-		marginBottom: '16px',
-		fontFamily: 'Popins',
-		textAlign: 'center',
-	},
-	button: {
-		margin: '16px 0',
-		fontFamily: 'Popins',
-		width: '60%',
-	},
-	incitingText: {
-		color: '#4128FF',
-		fontFamily: 'Popins',
-		marginLeft: '10px',
-		cursor: 'pointer',
-	},
+export const useStyles = makeStyles()((theme) => {
+	const colors = tokens(theme.palette.mode);
+	return {
+		root: {
+			textAlign: 'center',
+			fontFamily: 'Popins',
+			fontSize: '32px',
+		},
+		description: {
+			marginBottom: '16px',
+			fontFamily: 'Popins',
+			textAlign: 'center',
+		},
+		input: {
+			'& label.Mui-focused': {
+				color: colors.secondary.DEFAULT,
+			},
+			'& .MuiOutlinedInput-root': {
+				'&.Mui-focused fieldset': {
+					borderColor: colors.secondary.DEFAULT,
+				},
+			},
+		},
+		button: {
+			margin: '16px 0',
+			fontFamily: 'Popins',
+			width: '60%',
+		},
+	};
 });
